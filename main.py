69bee6602ec3f89c91c09da0546ede56a0fa03c2
WebberWu <nmmn1501@gmail.com>
2018-06-24 14:28:29 +0800

    fix

--- main.py
+++ main.py
@@ -48,5 +48,5 @@ def handle_message(event):
 
 if __name__ == "__main__":
     # port = int(os.environ.get('PORT', 5000))
-    # app.run(host='0.0.0.0', port=port)
-    app.run()
+    app.run(host='0.0.0.0', port=33507)
+    # app.run()
